8fba39c03c952b1bb311f8e5e23b14c269847ba1
Taylor Mott <trm36@humboldt.edu>
2014-09-27 12:18:57 -0600

    Toolbar -- In class project Completed
    tool bar in xib with four buttons with variable width spacing
    
    mail - creates a new email to mail
    red - changes background to red
    blue - changes background to blue
    new - presents a custom view controller with button to dismiss
    
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- OptionsToolbar/OTViewController.m
+++ OptionsToolbar/OTViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "OTViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface OTViewController ()
+@interface OTViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -25,5 +26,54 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)mailButtonPressed:(UIBarButtonItem *)sender
+{
+    MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc]init];
+    
+    mailViewController.mailComposeDelegate = self;
+    
+    [mailViewController setSubject:@"my subject"];
+    [mailViewController setMessageBody:@"<strong>message</strong> <em>body</em>" isHTML:YES];
+    
+    [self presentViewController:mailViewController animated:NO completion:nil];
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [controller dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)redButtonPressed:(UIBarButtonItem *)sender
+{
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)blueButtonPressed:(UIBarButtonItem *)sender
+{
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (IBAction)newButtonPressed:(UIBarButtonItem *)sender
+{
+    UIViewController *customViewController = [UIViewController new];
+    
+    customViewController.view.backgroundColor = [UIColor blackColor];
+    
+    UIButton *customVCButton = [[UIButton alloc] initWithFrame:CGRectMake(0, (customViewController.view.frame.size.height - 20)/2, customViewController.view.frame.size.width, 20)];
+    [customVCButton setTitle:@"HAPPY BIRTHDAY REN!" forState:UIControlStateNormal];
+    //[customVCButton setTintColor:[UIColor redColor]];
+    [customVCButton addTarget:self action:@selector(dismissCustomView) forControlEvents:UIControlEventTouchUpInside];
+    
+    [customViewController.view addSubview:customVCButton];
+    
+    [self presentViewController:customViewController animated:YES completion:nil];
+}
+
+- (void)dismissCustomView
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
